5a6b383ad7e60f911d657c481f10cf601612a451
Rokoned2 <rokoned2@gmail.com>
2021-09-15 13:11:00 -0500

    navigation fixed

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-const classNameName = () => {
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navigation">
       <input
@@ -9,43 +11,49 @@ const classNameName = () => {
         id="navi-toggle"
       />
 
-      <label for="navi-toggle" className="navigation__button">
+      <label
+        for="navi-toggle"
+        className="navigation__button"
+        onClick={() => setOpen(!open)}
+      >
         <span className="navigation__icon">&nbsp;</span>
       </label>
 
       <div className="navigation__background">&nbsp;</div>
 
       <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <a href="#section-about" className="navigation__link">
-              <span>01</span>Sobre Trava
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#section-features" className="navigation__link">
-              <span>02</span>Tus beneficios
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#section-tours" className="navigation__link">
-              <span>03</span>Tours populares
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#section-stories" className="navigation__link">
-              <span>04</span>Historias
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#section-book" className="navigation__link">
-              <span>05</span>Agendar ahora
-            </a>
-          </li>
-        </ul>
+        {open ? (
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a href="#section-about" className="navigation__link">
+                <span>01</span>Sobre Trava
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#section-features" className="navigation__link">
+                <span>02</span>Tus beneficios
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#section-tours" className="navigation__link">
+                <span>03</span>Tours populares
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#section-stories" className="navigation__link">
+                <span>04</span>Historias
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#section-book" className="navigation__link">
+                <span>05</span>Agendar ahora
+              </a>
+            </li>
+          </ul>
+        ) : null}
       </nav>
     </div>
   );
 };
 
-export default classNameName;
+export default Navbar;
